ee79f21d639bd1617dfc444b69e6db587d281718
evshiron <evshiron@gmail.com>
2016-05-11 17:01:41 +0800

    feat(bin): provide dummy executable

---
+++ bin/nwd.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node --harmony-destructuring --harmony-rest-parameters
+
+'use strict';
+
+const commander = require('commander');
+
+const NWD = require('../');
+
+commander.version(require('../package.json').version);
+
+commander.command('*')
+.action(() => commander.help());
+
+commander.command('list')
+.action(NWD.commands.list);
+
+commander.command('latest')
+.action(NWD.commands.latest);
+
+commander.command('stable')
+.action(NWD.commands.stable);
+
+commander.command('caches')
+.action(NWD.commands.caches);
+
+commander.command('download')
+.option('-v,--version <version>')
+.option('-p,--platform <platform>')
+.option('-a,--arch <arch>')
+.option('-f,--flavor <flavor>')
+.action(NWD.commands.download);
+
+if(process.argv.length <= 2) {
+    commander.help();
+}
+
+commander.parse(process.argv);
